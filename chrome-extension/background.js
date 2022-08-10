81fc799b1040420ab22475510e4b77dfe985db95
Mohammad Farseen Manekhan <67449931+farseenmanekhan1232@users.noreply.github.com>
2022-08-10 23:31:23 +0530

    Added SessionStorage Feature
    Now It can Switch between the Actual View and Decluttered View

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,14 +1,37 @@
 function reddenPage() {
-  document.getElementById("col1").remove();
-  document.getElementById("col2").remove();
-  document.getElementById("header").remove();
-  document.getElementById("page_margins").style["max-width"] = "123em";
+  if (window.sessionStorage.getItem("declutterOnlineJudge") == "no") {
+    window.sessionStorage.setItem("declutterOnlineJudge", "yes");
+    document.getElementById("col1").style["display"] = "none";
+    document.getElementById("col2").style["display"] = "none";
+    document.getElementById("header").style["display"] = "none";
+    document.getElementById("page_margins").style["max-width"] = "123em";
 
-  document.getElementById("col3").style["margin-right"] = "0";
-  document.getElementById("col3").style["margin-left"] = "0";
-document.getElementById("col3_content_wrapper").querySelector("iframe").style["height"]="100vh";
+    document.getElementById("col3").style["margin-right"] = "0";
+    document.getElementById("col3").style["margin-left"] = "0";
+    document
+      .getElementById("col3_content_wrapper")
+      .querySelector("iframe").style["height"] = "100vh";
+  } else {
+    window.sessionStorage.setItem("declutterOnlineJudge", "no");
+    document.getElementById("col1").style["display"] = "block";
+    document.getElementById("col2").style["display"] = "block";
+    document.getElementById("header").style["display"] = "block";
+    document.getElementById("page_margins").style["max-width"] = "80em";
 
+    document.getElementById("col3").style["margin-right"] = "20%";
+    document.getElementById("col3").style["margin-left"] = "20%";
+    document
+      .getElementById("col3_content_wrapper")
+      .querySelector("iframe").style["height"] = "80vh";
+  }
 }
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let url = tabs[0].url;
+  console.log(url);
+  window.sessionStorage("declutterOnlineJudge", "yes");
+  window;
+});
 
 chrome.action.onClicked.addListener((tab) => {
   if (tab.url.includes("https://onlinejudge.org/")) {
